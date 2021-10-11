51e7db50748178d6e59ef13d9814ac6280cc49d4
Ningthangom <ningthangom@gmail.com>
2021-10-11 14:45:10 +1030

    added hooks state and props

--- components/todo.js
+++ components/todo.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 
 const Todo = (props) => {
     return (
-        <View>
-            <Text >{props.name} </Text>
+        <View  style= {[styles.item, {margin: 8, padding: 8}]} >
+            <Text>
+                {props.item} 
+              
+            </Text>
+            <Button
+             title = {'Delete'}
+             color= {'red'}
+             onPress = {() => props.delete(props.item)} /> 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderColor: 'red',
+        borderWidth:1,
+        borderRadius: 5,
+        backgroundColor: 'whitesmoke'
+    }
+})
 
 export default Todo;
--- components/todoList.js
+++ components/todoList.js
@@ -1,21 +1,67 @@
 
-import React, {useState} from 'react';
-import { View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, ScrollView, TextInput } from 'react-native';
 import Todo from './todo'
 
 const TodoList = () => {
     const [title, setTitle] = useState('Todo-List');
-    return (
-        <View>
+    const [text, setText] = useState('');
+    const [list, setList] = useState(['Hello Wold']);
 
-            <Text>{title}</Text>
-            <Todo name = {'First Todo'} />
-            <Todo name = {'Second Todo'}/>
-          {/*   <Todo name = {'third Todo'} /> */}
-            <Button title = 'Change me' onPress = {()=> setTitle('My Lists')}/>
+
+    // add item method
+    const addItem = () => {
+        const updatedList = list;
+        updatedList.push(text);
+        setList(updatedList);
+        setText('');
+    }
+
+    // delete item method
+    const deleteItem = (index) => {
+        const updatedList = list.filter((todo) => todo !== index);
+        setList(updatedList);
+    }
+    return (
+        <View style={{ width: '80%', marginButton: 60 }}>
+
+            <Text style={[styles.align, styles.font]} >{title}</Text>
+            <ScrollView>
+                {list.map((x, index) =>(
+                    <Todo key={index}
+                     item={x} index={index} delete={deleteItem} />))}
+            </ScrollView>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    value={text}
+                    onChangeText={(text) => setText(text)}
+                />
+                <Button title='Add Item' onPress={addItem} />
+            </View>
+
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    align: {
+        alignSelf: 'center',
+    },
+    font: {
+        fontSize: 20,
+        fontWeight: 'bold',
+       
+
+    },
+    input: {
+        borderRadius: 5,
+        borderWidth: 1,
+        marginBottom: 8,
+        padding: 8,
+        borderColor: 'blue'
+    }
+})
 
 export default TodoList;
